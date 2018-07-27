172633e1617271df2e8d371adc59ef8c9c85e96c
Alexjairo <jairomorocho@gmail.com>
2018-07-27 05:16:13 -0500

    nuevo

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+ 
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
-export class AboutPage {
+export class AboutPage implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map; 
 
   constructor(public navCtrl: NavController) {
 
   }
+  ngOnInit() {
+    this.startGoogleMap();
+  }
 
+  startGoogleMap() {
+    const Ubicacion =new google.maps.LatLng(-0.273587, -78.546235);
+    const mapProp= {
+  
+        center: Ubicacion,
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    const map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    var Marcador = new google.maps.Marker({
+    position:Ubicacion,
+    map:map,
+    
+   });
+  
+  }
+  
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController,  AlertController } from 'ionic-angular';
 import { RegistrarsePage } from '../registrarse/registrarse';
 import { IngresarPage } from '../ingresar/ingresar';
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'page-home',
@@ -14,9 +15,59 @@ export class HomePage {
   }
 Regist(){
     let mialerta = this.registrar.create({
-      title:'titulo de alerta',
-      message:'mensaje',
-      buttons:['entendido']
+      title :'DATOS',
+      message:'Todos los campos son requeridos',
+      inputs: [
+        {
+          name: 'nombres',
+    
+          placeholder: 'nombre',
+        },
+        {
+          name : 'apellidos',
+          placeholder: 'apellidos',
+        
+        },
+        {
+          name: 'CI',
+    
+          placeholder: 'C.I',
+        },
+        {
+          name: 'correo',
+    
+          placeholder: 'correo electronico ejmpl@gmail.com',
+        },
+        {
+          name: 'cel',
+    
+          placeholder: 'numero de celular',
+        },
+        {
+          name : 'curso',
+          placeholder: 'cuso ejm: 1-2-3-4',
+        },
+
+        {
+          name : 'paralelo',
+          placeholder: 'paralelo ejm:A-B-C',
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'guardar',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
     });
   mialerta.present();
   // this.navCtrl.push( RegistrarsePage );
@@ -25,9 +76,34 @@ Ingresar(){
   let mialerta = this.entrar.create({
     title:'titulo de alerta',
     message:'mensaje',
-    buttons:['entendido']
-  });
+    inputs: [
+      {
+        name: 'correo',
+  
+        placeholder: 'Usuario/correo electronico',
+      },
+      {
+        name: 'clave',
+  
+        placeholder: 'contraseña',
+      },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'guardar',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+
 mialerta.present();
 }
-
 }
